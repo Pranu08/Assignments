a7db238bf1cc6187626d11e0e7ab7946b806ec2a
Pranu08 <pranitaparatane008@gmail.com>
2026-08-10 23:37:36 +0530

    Delete QT3.py

--- QT3.py
+++
@@ -1,9 +0,0 @@
-#QT3. Convert distant given in feet and inches into meter and centimeter.
-
-feet = float(input("Enter feet:"))
-inches = float(input("Enter inches:"))
-
-cm = (feet * 30.45) + (inches * 2.54)
-
-print("Meter =", cm / 100)
-print("Centimeter=", cm)

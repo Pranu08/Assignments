6ed3ba2126aa676abc18a6a32f38bf40a3a7c2be
Pranu08 <pranitaparatane008@gmail.com>
2026-08-10 23:36:14 +0530

    Delete QT10.py

--- QT10.py
+++
@@ -1,7 +0,0 @@
-#QT10.Write a program to reverse three-digit number.
-
-num = int(input("Enter 3 digit number: "))
-
-rev = (num % 10) * 100 + ((num // 10) % 10) * 10 + num // 100
-
-print("Reverse=", rev)

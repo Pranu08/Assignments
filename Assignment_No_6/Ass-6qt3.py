ec117e4b06d24c8f906bad5a1e8aabf73be527ee
Pranu08 <pranitaparatane008@gmail.com>
2026-08-13 02:20:13 +0530

    Add files via upload

---
+++ Assignment_No_6/Ass-6qt3.py
@@ -0,0 +1,14 @@
+#C]
+
+n = 4
+
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    
+    for j in range(1, i + 1):
+        if j == 1 or j == i:
+            print(1, end=" ")
+        else:
+            print(i - 1, end=" ")
+    
+    print()
---
+++ Assignment_No_6/Ass-6qt5.py
@@ -0,0 +1,7 @@
+#E]
+
+n = 5
+
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    print("* " * (2 * i - 1))
